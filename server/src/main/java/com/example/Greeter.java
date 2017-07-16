b3897f5af7d71d6c3f55cd001eaad002ae521ac0
jmsamudio-windows <juanmanuelsamudio@gmail.com>
2017-07-16 10:34:57 -0400

    primer commit desde IntellijIDEA

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   *
+   * @param String
+   * @return
+   */
+  public final String greet(Final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by JMSamudio on 16/7/2017.
+ */
+package com.example;
